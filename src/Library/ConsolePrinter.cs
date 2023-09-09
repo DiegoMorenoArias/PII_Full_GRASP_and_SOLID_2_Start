cbe1eceb03c0018f9056096ff5e4be85d406f783
Diego Moreno <diego.morenoa@correo.ucu.edu.uy>
2023-09-09 18:36:32 -0300

    Ejercicio terminado

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Full_GRASP_And_SOLID.Library
+{
+
+    public class ConsolePrinter
+    {
+        public static void PrintRecipe(Recipe recipe)
+        {
+            Console.WriteLine($"Receta de {recipe.FinalProduct.Description}:");
+            foreach (Step step in recipe.steps)
+            {
+                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+            }
+        }
+    }
+    // Para implementar este cambio se utiliza el principio SRP y el Expert.
+    // SRP: La clase tiene como única responsabilidad imprimir la receta
+    // Expert: La clase tiene la suficiente información (el objeto Recipe recibido) para realizar todo lo que tiene que hacer.
+}
